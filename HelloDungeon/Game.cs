c132d34341ee1abf690ee4b95d867e8638cce560
Jurhell hughes <somethinghughes@gmail.com>
2023-08-23 15:07:28 -0500

    Added welcome message and player stats

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -10,6 +10,27 @@ namespace HelloDungeon
     {
         public void Run()
         {
+            //Stat concepts (most likely aren't final)
+            int mana = 70;
+            string playerName = "Tom";
+            bool attackIsDodged = true;
+            float dodgeChance = 12.0f;
+            string className = "Mage";
+
+            //Stop mashing on wakeup (Print out advise)
+            Console.WriteLine("Just Block LMAO");
+            Console.WriteLine("Git Good");
+
+            //Get player input for first and last name
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            playerName = firstName + lastName;
+
+            //Welocome to the real world
+            Console.WriteLine("Welcome" +" "+ playerName);
+            Console.WriteLine("To a world of mortgages and late fees");
+            
+            
 
         }
     }
